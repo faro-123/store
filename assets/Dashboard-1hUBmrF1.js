327d68167bd7d274ab5229a42a9a6a7687af98cc
faro-123 <faro-123@users.noreply.github.com>
2026-07-11 08:22:24 +0000

    deploy: 863976039d28bcac5e4b60e09f53796823942833

--- assets/Dashboard-1hUBmrF1.js
+++ assets/Dashboard-1hUBmrF1.js
@@ -1,4 +1,4 @@
-import{c as M,j as e,r as c,T as H,X as B,a as k,S as V}from"./index-BIXgafnu.js";/**
+import{c as M,j as e,r as c,T as H,X as B,a as k,S as V}from"./index-Brse1oC_.js";/**
  * @license lucide-react v0.468.0 - ISC
  *
  * This source code is licensed under the ISC license.
